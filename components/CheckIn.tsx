a13175c145c7d83214bfbf826767553a02860483
JoshMuasya <muasyajoshua07@gmail.com>
2025-03-14 22:19:04 +0300

    Made some few changes

--- components/CheckIn.tsx
+++ components/CheckIn.tsx
@@ -1,18 +1,12 @@
 'use client'
 
-import React, { useState } from 'react'
 import CheckInInterface from './CheckInInterface'
-import { Guest } from '@/types';
 
 const CheckIn = () => {
-    const [guests, setGuests] = useState<Guest[]>([]);
 
     return (
         <div>
-            <CheckInInterface
-                guests={guests}
-                onCheckIn={(id) => setGuests(guests.map((g) => (g.id === id ? { ...g, checkInStatus: "checked-in" } : g)))}
-            />
+            <CheckInInterface />
         </div>
     )
 }
